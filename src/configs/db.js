8f196e69dd1e650af3425687a8361be111bb2078
jcdev21 <jcdev21@gmail.com>
2020-12-29 23:53:58 +0700

    change config database

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,9 +2,10 @@ const Sequelize = require('sequelize');
 
 let db;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     db = new Sequelize(process.env.PROD_DATABASE, process.env.PROD_USER, process.env.PROD_PASSWORD, {
-        dialect: process.env.PROD_DATABASE_DIALECT
+        dialect: process.env.PROD_DATABASE_DIALECT,
+        host: process.env.PROD_HOST,
     });
 } else {
     db = new Sequelize(process.env.DEV_DATABASE, process.env.DEV_USER, process.env.DEV_PASSWORD, {
